e2dacf5e9975081f807584a6ea08b95ee300b0a4
liuhuo-196 <66284484+liuhuo-196@users.noreply.github.com>
2020-07-31 11:29:31 +0800

    Add files via upload

---
+++ logs.sql
@@ -0,0 +1,65 @@
+/*
+SQLyog Ultimate v12.3.1 (64 bit)
+MySQL - 8.0.20 : Database - logs
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`logs` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `logs`;
+
+/*Table structure for table `logs` */
+
+DROP TABLE IF EXISTS `logs`;
+
+CREATE TABLE `logs` (
+  `logId` int NOT NULL AUTO_INCREMENT,
+  `logs` varchar(255) NOT NULL,
+  `createTime` date NOT NULL,
+  PRIMARY KEY (`logId`)
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `logs` */
+
+insert  into `logs`(`logId`,`logs`,`createTime`) values 
+(2,'fsdht','2020-07-16'),
+(7,'555','2014-05-12'),
+(9,'adas','2020-04-21'),
+(10,'46546465413231','1996-06-23'),
+(11,'asdasf','2012-12-22'),
+(17,'asdagv','2020-07-29'),
+(22,'25331mmjhmglog','2020-07-30'),
+(23,'asdlkanfps;l;slog','2020-07-30'),
+(26,'165768','2020-07-30'),
+(27,'使肌肤443523','2020-07-31');
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `userId` int NOT NULL AUTO_INCREMENT,
+  `userName` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`userId`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `user` */
+
+insert  into `user`(`userId`,`userName`,`password`) values 
+(1,'Alice','123456'),
+(2,'zhangsan','123456'),
+(3,'admin','123456');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
